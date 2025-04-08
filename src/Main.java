871f8a63517b79fe10dfbc85b65dd3dabfd022be
ThrillerGriller <thrillergriller1234@gmail.com>
2025-04-08 02:06:16 -0400

    Patch v0.2
    - Updated GUI

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.*;
 
 public class Main {
     static boolean[][] visited;
-    static int pathLength = -1;
 
     static char[][] map;
 
     static char[] mapLegend = {'B', 'O', 'X', 'S'};
 
+
+    /*
+     * Path finding algorithm variables
+     */
+
+    static boolean canExit = false;
+    static ArrayList<Integer> pathX = new ArrayList<Integer>();
+    static ArrayList<Integer> pathY = new ArrayList<Integer>();
+
+    static int startX, startY;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        initGUI();
+        startMenuGUI();
 
         System.out.println("Ohayo gozaimasu");
 
@@ -38,38 +50,142 @@ public class Main {
             }
             System.out.println();
         }
+        dfs(startX, startY);
+
+        for (int i = 0; i < pathX.size(); i++) {
+            System.out.println(pathY.get(i) + " " + pathX.get(i));
+        }
     }
 
-    public static void initGUI() {
+    public static void startMenuGUI() {
         JFrame welcomeFrame = new JFrame();
         welcomeFrame.setTitle("Sam & Edi Maze Assignment 2025");
         welcomeFrame.setSize(800,500);
-        welcomeFrame.setLayout(new BoxLayout(welcomeFrame.getContentPane(),BoxLayout.Y_AXIS));
+        welcomeFrame.setLayout(new GridBagLayout());
+        welcomeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GridBagConstraints gbc = new GridBagConstraints();
 
         JLabel preTitle = new JLabel();
         preTitle.setText("Welcome to the Sam & Edi");
         preTitle.setFont(new Font("Roboto", Font.PLAIN, 24));
         preTitle.setHorizontalAlignment(JLabel.CENTER);
         preTitle.setVerticalAlignment(JLabel.CENTER);
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        welcomeFrame.add(preTitle, gbc);
+
         JLabel title = new JLabel();
         title.setText("Maze Assignment 2025");
         title.setFont(new Font("Roboto", Font.BOLD, 28));
         title.setHorizontalAlignment(JLabel.CENTER);
         title.setVerticalAlignment(JLabel.CENTER);
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        welcomeFrame.add(title, gbc);
+
+        JButton mazeButton = new JButton();
+        mazeButton.setText("Generate Random Maze");
+        mazeButton.setFont(new Font("Roboto", Font.BOLD, 14));
+        mazeButton.setHorizontalAlignment(JLabel.CENTER);
+        mazeButton.setVerticalAlignment(JLabel.CENTER);
+        gbc.gridy = 2;
+        gbc.gridwidth = 1;
+        gbc.insets = new Insets(30,0,0,0);
+
+        mazeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mazeSettingGUI("Generate Random Maze");
+            }
+        } );
+
+        welcomeFrame.add(mazeButton, gbc);
+
+        JButton randMazeButton = new JButton();
+        randMazeButton.setText("Generate Maze (Purely Random)");
+        randMazeButton.setFont(new Font("Roboto", Font.BOLD, 14));
+        randMazeButton.setHorizontalAlignment(JLabel.CENTER);
+        randMazeButton.setVerticalAlignment(JLabel.CENTER);
+        gbc.gridy = 3;
+        gbc.gridwidth = 1;
+        gbc.insets = new Insets(20,0,0,0);
+
+        randMazeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mazeSettingGUI("Generate Maze (Purely Random)");
+            }
+        } );
+
+        welcomeFrame.add(randMazeButton, gbc);
+
+        JButton readFileButton = new JButton();
+        readFileButton.setText("Generate Maze (File)");
+        readFileButton.setFont(new Font("Roboto", Font.BOLD, 14));
+        readFileButton.setHorizontalAlignment(JLabel.CENTER);
+        readFileButton.setVerticalAlignment(JLabel.CENTER);
+        gbc.gridy = 4;
+        gbc.gridwidth = 1;
+        gbc.insets = new Insets(20,0,0,0);
+        welcomeFrame.add(readFileButton, gbc);
 
         JPanel titlePanel = new JPanel();
         titlePanel.setBackground(Color.GREEN);
-        titlePanel.add(preTitle);
 
-        welcomeFrame.add(preTitle);
-        welcomeFrame.add(title);
         welcomeFrame.setVisible(true);
+    }
 
+    public static void mazeSettingGUI(String currentSetting) {
+        JFrame settingsFrame = new JFrame();
+        settingsFrame.setTitle("Maze Settings");
+        settingsFrame.setSize(300,200);
+        settingsFrame.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        JLabel rowsText = new JLabel();
+        rowsText.setText("Rows:");
+        rowsText.setFont(new Font("Roboto", Font.BOLD, 14));
+        rowsText.setHorizontalAlignment(JLabel.CENTER);
+        rowsText.setVerticalAlignment(JLabel.CENTER);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.insets = new Insets(0,0,0,0);
+        settingsFrame.add(rowsText, gbc);
+
+        SpinnerModel rowModel = new SpinnerNumberModel(5, 3, 20, 1);
+        JSpinner rowSpinner = new JSpinner(rowModel);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.insets = new Insets(0,0,0,0);
+        settingsFrame.add(rowSpinner, gbc);
+
+        JLabel colsText = new JLabel();
+        colsText.setText("Columns:");
+        colsText.setFont(new Font("Roboto", Font.BOLD, 14));
+        colsText.setHorizontalAlignment(JLabel.CENTER);
+        colsText.setVerticalAlignment(JLabel.CENTER);
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.insets = new Insets(0,20,0,0);
+        settingsFrame.add(colsText, gbc);
+
+        SpinnerModel colModel = new SpinnerNumberModel(5, 3, 20, 1);
+        JSpinner colSpinner = new JSpinner(colModel);
+        gbc.gridx = 3;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.insets = new Insets(0,0,0,0);
+        settingsFrame.add(colSpinner, gbc);
+
+        settingsFrame.setVisible(true);
     }
 
     public static void generateMap(int rows, int cols) {
-        ArrayList<Integer> openX = new ArrayList<Integer>();
-        ArrayList<Integer> openY = new ArrayList<Integer>();
+        ArrayList<Integer> openX = new ArrayList<>();
+        ArrayList<Integer> openY = new ArrayList<>();
 
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
@@ -80,6 +196,9 @@ public class Main {
         openX.add((int) (Math.random() * (cols - 2)) + 1); // Generate Start X Position
         openY.add((int) (Math.random() * (rows - 2)) + 1); // Generate Start Y Position
         map[openY.get(0)][openX.get(0)] = 'S';
+
+        startX = openX.get(0);
+        startY = openY.get(0);
 
         boolean hasExit = false;
 
@@ -139,11 +258,10 @@ public class Main {
                 }
             }
             if (builtNewPoint) {
-                //
-                if (openX.getLast() == 0 || openX.getLast() == cols - 1 || openY.getLast() == 0 || openY.getLast() == rows - 1) {
-                    map[openY.getLast()][openX.getLast()] = 'X';
-                    openY.removeLast();
-                    openX.removeLast();
+                if (openX.get(openX.size() - 1) == 0 || openX.get(openX.size() - 1) == cols - 1 || openY.get(openY.size() - 1) == 0 || openY.get(openY.size() - 1) == rows - 1) {
+                    map[openY.get(openY.size() - 1)][openX.get(openX.size() - 1)] = 'X';
+                    openY.remove(openY.size() - 1);
+                    openX.remove(openX.size() - 1);
                     hasExit = true;
                 }
             } else {
@@ -200,33 +318,36 @@ public class Main {
         return countAdjacent <= 1;
     }
 
-    public static void dfs(char map[][], int x, int y, int counter) {
-        if (map[y][x] == 'X') {
-            pathLength = counter;
+    public static void dfs(int posX, int posY) {
+    	if (canExit) {
+    		return;
+    	}
+        if (map[posY][posX] == 'X') {
+            canExit = true;
+            return;
         }
-        if (map[y][x - 1] == 'O' || map[y][x - 1] == 'X') {
-            if (visited[y][x - 1] == false) {
-                visited[y][x - 1] = true;
-                dfs(map, x - 1, y, counter + 1);
-            }
+        visited[posY][posX] = true;
+        pathX.add(posX);
+    	pathY.add(posY);
+        if (map[posY][posX] != 'S') {
+        	//Update GUI here
+        	// GUI map[posY][posX] = '+'
         }
-        if (map[y][x + 1] == 'O' || map[y][x + 1] == 'X') {
-            if (visited[y][x + 1] == false) {
-                visited[y][x + 1] = true;
-                dfs(map, x + 1, y, counter + 1);
-            }
+        if (map[posY][posX - 1] != 'B' && !visited[posY][posX - 1]) {
+            dfs(posX - 1, posY);
         }
-        if (map[y - 1][x] == 'O' || map[y - 1][x] == 'X') {
-            if (visited[y - 1][x] == false) {
-                visited[y - 1][x] = true;
-                dfs(map, x, y - 1, counter + 1);
-            }
+        if (map[posY][posX + 1] != 'B' && !visited[posY][posX + 1]) {
+            dfs(posX + 1, posY);
         }
-        if (map[y + 1][x] == 'O' || map[y + 1][x] == 'X') {
-            if (visited[y + 1][x] == false) {
-                visited[y + 1][x] = true;
-                dfs(map, x, y + 1, counter + 1);
-            }
+        if (map[posY - 1][posX] != 'B' && !visited[posY - 1][posX]) {
+            dfs(posX, posY - 1);
+        }
+        if (map[posY + 1][posX] != 'B' && !visited[posY + 1][posX]) {
+            dfs(posX, posY + 1);
+        }
+        if (!canExit) {
+            pathX.remove(pathX.size() - 1);
+            pathY.remove(pathX.size() - 1);
         }
     }
 }
